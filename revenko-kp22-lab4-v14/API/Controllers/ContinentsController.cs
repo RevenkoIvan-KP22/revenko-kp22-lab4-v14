3d176b59a1191662ce7154ef3babebc9864efc71
RevenkoIvan-KP22 <revenko.ivan.121@gmail.com>
2024-04-11 13:49:10 +0300

    Continents implemented.

--- revenko-kp22-lab4-v14/API/Controllers/ContinentsController.cs
+++ revenko-kp22-lab4-v14/API/Controllers/ContinentsController.cs
@@ -1,10 +1,51 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using System.Text;
 
 namespace API.Controllers {
     [Route("api/[controller]")]
     [ApiController]
-    public class ContinentsController : ControllerBase {
+    public class ContinentsController : ControllerBase, ISender {
+        private readonly IConnectionFactory _connectionFactory;
+        private string[] _continents = { "Asia", "Africa", "NA", "SA", "Antarctica", "Europe", "Australia" };
+        public ContinentsController(IConnectionFactory connectionFactory) {
+            _connectionFactory = connectionFactory;
+        }
 
+        [HttpGet]
+        public async Task<IActionResult> GetContinent(string continent) {
+            try {
+                if (!_continents.Contains(continent)) {
+                    return NotFound($"You probably live on Mars.");
+                }
+
+                string message = String.Concat(_continents[Random.Shared.Next(_continents.Length)], " says: Hello!");
+
+                return await SendMessage(message, continent);
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        public async Task<IActionResult> SendMessage(string message, string rKey) {
+            try {
+                using var connection = _connectionFactory.CreateConnection();
+                using var channel = connection.CreateModel();
+
+                var body = Encoding.UTF8.GetBytes(message);
+                string messageToSend = $"Message ID: {new Guid()}\nDate: {DateTime.Now}\nContent: {message}";
+
+                channel.ExchangeDeclare(exchange: "cont", type: ExchangeType.Direct);
+                channel.BasicPublish(exchange: "cont", routingKey: $"continent.{rKey}", body: body);
+                await Console.Out.WriteLineAsync($"{message} - continent.{rKey}");
+
+                return Ok("Success.");
+            }
+            catch (Exception ex) {
+                return StatusCode(502, ex.Message);
+            }
+        }
     }
 }
